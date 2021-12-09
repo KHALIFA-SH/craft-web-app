9ca5b8d42fcaca5fb9fcea15ecf221ee61dfb900
Kshk <kelifa.shk@craft.edu>
2021-12-09 13:00:19 -0500

    first commit

---
+++ src/main/java/com/craftsoft/edu/CraftSoftFullStackAngularSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.craftsoft.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftSoftFullStackAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftSoftFullStackAngularSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/craftsoft/edu/CraftSoftFullStackAngularSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftsoft.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftSoftFullStackAngularSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
